80f16d7da8245cdd29b7031968578423a9222456
Sudha Arunkumar <sstsudha@gmail.com>
2018-07-01 17:46:53 +0530

    First checin

---
+++ NumberCount/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="NumberCount._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div>
+        <asp:Button ID="btnNumberCount" runat ="server" Text="NumberCount" OnClick="btnNumberCount_Click" /></br>
+        <asp:Label ID="lblTotalCount" runat ="server" Visible ="false">Total Count: </asp:Label> <asp:Label ID="lblcount" runat="server" Visible ="false"></asp:Label>
+    </div>
+</asp:Content>
---
+++ NumberCount/Default.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NumberCount
+{
+    public partial class _Default : Page
+    {
+
+        protected void btnNumberCount_Click(object sender, EventArgs e)
+        {
+            string[] args = null;
+            NumbertoWordConverstion(args);
+        }
+
+        private void NumbertoWordConverstion(string[] args)
+        {
+
+            int numberOfLetters = 0;
+
+            for (int i = 1; i <= 1000; i++)
+                numberOfLetters += LetterConverstion(i);
+
+            lblTotalCount.Visible = true;
+            lblcount.Visible = true;
+            lblcount.Text = numberOfLetters.ToString();
+
+        }
+
+        private int LetterConverstion(int number)
+        {
+            try
+            {
+                string[] ones = new string[] { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+                string[] onesofOne = new string[] { "", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                string[] tens = new string[] { "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number == 1000)
+                    return "onethousand".Length;
+                string lettterString = "";
+                int[] letters = new int[3];
+
+                letters[0] = number / 100;
+                letters[1] = (number / 10) % 10;
+                letters[2] = number % 10;
+
+                if (letters[0] > 0)
+                {
+                    lettterString += ones[letters[0]] + "hundred";
+                    if (letters[1] != 0 || letters[2] != 0)
+                        lettterString += "and";
+                }
+
+                if (letters[1] == 1)
+                {
+                    if (letters[2] == 0)
+                        lettterString += tens[letters[1]];
+                    else
+                        lettterString += onesofOne[letters[2]];
+                }
+                else
+                    lettterString += tens[letters[1]] + ones[letters[2]];
+
+                return lettterString.Length;
+
+            }
+
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+     }
+  }
+ 
+       
+    
